316bebdf07f42af19cf26ca4be8bb8a8d601ad23
Rushaan Anwar <hunterczc02@gmail.com>
2025-01-14 10:58:35 +0530

    Day 3 Completed

--- day3part2.py
+++ day3part2.py
@@ -1,38 +1,24 @@
 import re
 
-with open('inputs/day3test.txt') as file:
+with open('inputs/day3input.txt') as file:
     input_file = file.read()
-    print(input_file)
 
-    # regex of do()
-    enable_function = r'do()'
-    disable_function = r"don't()"
     mul_pattern = r'mul\(-?\d+,-?\d+\)'
-
     combined_pattern = re.compile(r'do\(\)|don\'t\(\)|mul\(-?\d+,-?\d+\)')
 
-    matches = re.findall(combined_pattern, input_file)
-    print(matches)
-
-    i = 1
-    disable_numbers = 0
+    multiplication = True # if multiplication is enabled or disabled
     total = 0
-    for i in range(len(matches) - 1):
+    matches = re.findall(combined_pattern, input_file)
 
-        total_numbers = re.findall(r'\d+', matches[i])
-        num1, num2 = map(int, total_numbers)
-        total+= (num1 * num2)
-        print(total)
+    for match in matches:
+        if match == "do()":
+            multiplication = True
+        elif match == "don't()":
+            multiplication = False
 
-        if matches[i-1] == disable_function:
-            numbers = re.findall(stripping, matches[i])
-            num1, num2 = map(int, numbers)
-            disable_numbers += (num1 * num2)
-            print(disable_numbers)
+        numbers = re.findall(r'\d+', match)
+        if numbers and multiplication:
+            num1,num2 = map(int, numbers)
+            total += num1 * num2
 
-
-
-
-
-
-    # regex of don't()
+    print(total)
